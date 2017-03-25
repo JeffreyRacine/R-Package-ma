04e48b36f8159fa322da580fb365b2da3e927b29
Jeffrey Racine <racinej@mcmaster.ca>
2017-03-25 10:47:00 -0400

    Initial setup

---
+++ R/lm.ma.R
@@ -0,0 +1,120 @@
+lm.ma <- function(y=NULL,
+                  X=NULL,
+                  X.eval=NULL,
+                  basis=c("glp","tensor","additive"),
+                  compute.deriv=TRUE,
+                  p.max=25,
+                  method=c("jma","mma"),
+                  ma.weights=NULL) {
+    
+    basis <- match.arg(basis)
+    method <- match.arg(method)
+
+    if(p.max < 2) stop("You must average over at least two models")
+    if(is.null(y) | is.null(X)) stop("You must provide data for y and X")
+    if(!is.null(X) & !is.null(X.eval) & NCOL(X)!=NCOL(X.eval)) stop("X and X.eval must contain the same number of predictors")
+
+    ## Divide into factors and numeric, if one regressor must be numeric
+
+    if(NCOL(X) > 1) {
+        xztmp <- crs:::splitFrame(data.frame(X))
+        x <- xztmp$x
+        z <- xztmp$z
+        if(!is.null(X.eval)) {
+            xztmp <- crs:::splitFrame(data.frame(X.eval))
+            xeval <- xztmp$x
+            zeval <- xztmp$z
+        } else {
+            xeval <- NULL
+            zeval <- NULL
+        }
+        rm(xztmp)
+    } else {
+        if(!is.numeric(X)) stop("Single predictor must be of type numeric")
+        x <- X
+        xeval <- X.eval
+        z <- NULL
+        zeval <- NULL
+    }
+
+    deriv <- NULL
+    if(compute.deriv) {
+        deriv.mat <- array(NA,c(if(is.null(X.eval)){NROW(X)}else{NROW(X.eval)},p.max,NCOL(X)))
+        deriv <- matrix(NA,if(is.null(X.eval)){NROW(X)}else{NROW(X.eval)},NCOL(X))
+    }
+
+    K <- numeric(length=p.max)
+    ma.mat <- matrix(NA,NROW(X),p.max)
+    fitted.mat <- matrix(NA,if(is.null(X.eval)){NROW(X)}else{NROW(X.eval)},p.max)
+
+    for(p in 1:p.max) {
+
+        model.ma <- suppressWarnings(crs:::predict.factor.spline(x,
+                                                                 y,
+                                                                 z,
+                                                                 K=cbind(rep(p,NCOL(x)),rep(1,NCOL(x))),
+                                                                 basis=basis)$model)
+
+        K[p] <- model.ma$rank
+
+        ma.mat[,p] <- Dmat.func(model.ma,method=method)
+
+        fitted.mat[,p] <- suppressWarnings(crs:::predict.factor.spline(x,
+                                                                  y,
+                                                                  z,
+                                                                  xeval=xeval,
+                                                                  zeval=zeval,
+                                                                  K=cbind(rep(p,NCOL(x)),rep(1,NCOL(x))),
+                                                                  basis=basis)$fitted.values[,1])
+
+        if(compute.deriv) {
+            for(k in 1:NCOL(X)) {
+                model.deriv <- suppressWarnings(crs:::deriv.factor.spline(x,
+                                                                          y,
+                                                                          z,
+                                                                          K=cbind(rep(p,NCOL(x)),rep(1,NCOL(x))),
+                                                                          xeval=xeval,
+                                                                          zeval=zeval,
+                                                                          knots="uniform",
+                                                                          basis=basis,
+                                                                          deriv.index=k,
+                                                                          deriv=1)[,1])
+        
+                deriv.mat[,p,k] <- model.deriv
+            }
+        }
+
+    }
+
+    if(is.null(ma.weights)) {
+
+        ## Largest dimensional model is the last
+        
+        sigsq <- summary(model.ma)$sigma^2
+        
+        ## Solve the quadratic program for the Mallows model average weights
+        
+        M <- ncol(ma.mat)
+        D <- t(ma.mat)%*%ma.mat
+        if(qr(D)$rank<M) D <- D + diag(1e-10,M,M)
+        A <- cbind(rep(1,M),diag(1,M,M))
+        b0 <- c(1,rep(0,M))
+        if(method=="mma") {
+            d <- -sigsq*K
+        } else {
+            d <- t(y)%*%ma.mat
+        }        
+        b <- solve.QP(Dmat=D,dvec=d,Amat=A,bvec=b0,meq=1)$solution
+
+    } else {
+        ## For bootstrapping use weights from initial call
+        b <- ma.weights
+    }
+
+    if(compute.deriv) for(k in 1:NCOL(X)) deriv[,k] <- deriv.mat[,,k]%*%b
+
+    return(list(fitted=fitted.mat%*%b,
+                deriv=deriv,
+                ma.weights=as.numeric(formatC(b,format="f",digits=4))))
+
+}
