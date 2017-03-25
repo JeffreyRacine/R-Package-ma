9ef489bbe15fd25f7a2611a905ddc50779f2f1ba
Jeffrey Racine <racinej@mcmaster.ca>
2017-03-25 17:53:38 -0400

    Doh

---
+++ R/util.R
@@ -0,0 +1,79 @@
+splitFrame <- function(xz, factor.to.numeric=FALSE) {
+  
+  if(missing(xz)) stop(" you must provide xz data")
+  if(!is.data.frame(xz)) stop(" xz must be a data frame")
+
+  xznames <- names(xz)
+  
+  IND <- logical()
+
+  for(i in 1:NCOL(xz)) IND[i] <- is.factor(xz[,i])
+
+  x <- xz[,!IND,drop=FALSE]
+  num.x <- ncol(x)
+
+  ## We require at least one continuous predictor to conduct spline
+  ## smoothing, but there may/may not be factors.
+
+  if(num.x == 0) stop(" can't fit spline surfaces with no continuous predictors")
+
+  xnames <- xznames[!IND]
+
+  is.ordered.z <- NULL
+  
+  if(any(IND)) {
+    is.ordered.z <- logical()
+    for(i in 1:NCOL(xz[,IND,drop=FALSE])) is.ordered.z[i] <- is.ordered((xz[,IND,drop=FALSE])[,i])
+    if(!factor.to.numeric) {
+      z <- data.frame(xz[,IND,drop=FALSE])
+    } else {
+      ## If factor.to.numeric crudely convert factors to numeric.
+      z <- matrix(NA,NROW(xz),NCOL(xz[,IND,drop=FALSE]))
+      ## To "revert" a factor f to its original numeric values,
+      ## as.numeric(levels(f))[f] is recommended. Problem is that for
+      ## character strings it produces a warning message. No idea how
+      ## to test for this so dropping for the moment. Will affect
+      ## ordered types.
+      for(i in 1:NCOL(xz[,IND,drop=FALSE])) {
+        suppressWarnings(z[,i] <- as.numeric(levels((xz[,IND,drop=FALSE])[,i]))[(xz[,IND,drop=FALSE])[,i]])
+        if(any(is.na(z[,i]))) z[,i] <- as.numeric((xz[,IND,drop=FALSE])[,i])
+      }
+    }
+    ## Don't assign names when factor.to.numeric is TRUE (otherwise
+    ## NAs populate matrix)
+    if(!factor.to.numeric) names(z) <- xznames[IND]
+    znames <- xznames[IND]
+    num.z <- ncol(z)
+  } else {
+    z <- NULL
+    znames <- NULL
+    num.z <- NULL
+  }
+
+  return(list(x=x,
+              num.x=num.x,
+              xnames=xnames,
+              z=z,
+              num.z=num.z,
+              is.ordered.z=is.ordered.z,
+              znames=znames))
+  
+}
+
+RSQfunc <- function(y,y.pred,weights=NULL) {
+  if(!is.null(weights)) {
+    y <- y*sqrt(weights)
+    y.pred <- y.pred*sqrt(weights)
+  }
+  y.mean <- mean(y)
+  return((sum((y-y.mean)*(y.pred-y.mean))^2)/(sum((y-y.mean)^2)*sum((y.pred-y.mean)^2)))
+}
+
+Dmat.func <- function(model,method=c("mma","jma")) {
+    method <- match.arg(method)
+    ## Residuals (Mallows model averaging)
+    if(method=="mma") return(residuals(model))
+    ## Jackknife fitted values (jackknife model averaging)
+    htt <- hatvalues(model)
+    if(method=="jma") return(fitted(model) - htt*residuals(model)/(1-htt))    
+}
