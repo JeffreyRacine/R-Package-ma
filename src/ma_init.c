9bdc5440d5ab03d6e7b8f3cbce385d48feeaa83d
Jeffrey Racine <racinej@mcmaster.ca>
2017-04-21 15:46:01 -0400

    R 3.4.0, registering native routines and disabling symbol search (good practice)

---
+++ src/ma_init.c
@@ -0,0 +1,24 @@
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .C calls */
+extern void gsl_bspline(void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void gsl_bspline_deriv(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void RuniqueCombs(void *, void *, void *, void *);
+
+static const R_CMethodDef CEntries[] = {
+    {"gsl_bspline",       (DL_FUNC) &gsl_bspline,        9},
+    {"gsl_bspline_deriv", (DL_FUNC) &gsl_bspline_deriv, 11},
+    {"RuniqueCombs",      (DL_FUNC) &RuniqueCombs,       4},
+    {NULL, NULL, 0}
+};
+
+void R_init_ma(DllInfo *dll)
+{
+    R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
