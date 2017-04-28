f9abfcd265c38b4a05d33577c6c555ce05d261b9
Jeffrey Racine <racinej@mcmaster.ca>
2017-04-28 16:32:38 -0400

    Minor demo tweak

--- demo/ma_np_rate.R
+++ demo/ma_np_rate.R
@@ -1,7 +1,7 @@
 ## Comparison of rates of convergence (RMSE) of model averaging
 ## versus nonparametric kernel regression (kernel rate is -2/(4+p) where p is 
-## the number of numeric predictors (-.33 for p=2), correctly specified 
-## parametric model's rate would be -1/2)
+## the number of numeric predictors (-0.33 for p=2), correctly specified 
+## parametric model's rate would be -0.50)
 
 library(ma)
 library(np)
