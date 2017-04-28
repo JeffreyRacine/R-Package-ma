7ed0d69478402b4ea33c14d72bc74abbb65b5277
Jeffrey Racine <racinej@mcmaster.ca>
2017-04-28 08:49:53 -0400

    More demos, slight changes

---
+++ demo/anova.R
@@ -0,0 +1,28 @@
+## Demonstration of anova-based signifance test
+
+library(ma)
+
+n <- 500
+x1 <- runif(n)
+x2 <- runif(n)
+
+## One irrelevant predictor (x2)
+
+dgp <- sin(2*pi*x1)
+y <- dgp + rnorm(n,sd=0.25*sd(dgp))
+model <- lm.ma(y~x1+x2,compute.anova=TRUE)
+summary(model)
+
+## Both predictors relevant
+
+dgp <- sin(2*pi*x1)+cos(2*pi*x2)
+y <- dgp + rnorm(n,sd=0.25*sd(dgp))
+model <- lm.ma(y~x1+x2,compute.anova=TRUE)
+summary(model)
+
+## Both predictors irrelevant
+
+y <- rnorm(n,sd=0.25*sd(dgp))
+model <- lm.ma(y~x1+x2,compute.anova=TRUE)
+summary(model)
+
---
+++ demo/linear.R
@@ -0,0 +1,32 @@
+## Demonstration of how one would replicate simple linear regression
+
+require(ma)
+
+set.seed(42)
+n <- 1000
+
+x1 <- rnorm(n)
+x2 <- rnorm(n)
+z1 <- rbinom(n,1,.5)
+z2 <- rbinom(n,1,.5)
+
+dgp <- x1 + x2 + z1 + z2
+
+y <- dgp + rnorm(n,sd=0.5*sd(dgp))
+
+z1 <- factor(z1)
+z2 <- factor(z2)
+
+model.lm <- lm(y~x1+x2+z1+z2)
+model.ma <- lm.ma(y~x1+x2+z1+z2,
+                  degree.min=1,
+                  degree.max=1,
+                  basis="additive",
+                  compute.deriv=TRUE,
+                  vc=FALSE)
+
+summary(model.lm)
+summary(model.ma)
+
+coef(model.lm)[-1]
+coef(model.ma)[1,]
