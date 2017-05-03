69d5193a0d4019e82241b6195b6b304ebd7b98f2
Jeffrey Racine <racinej@mcmaster.ca>
2017-05-03 11:47:08 -0400

    Taylor code very inefficient. Had used Somon Wood's tensor.product.model.matrix() and see he updated to call a C function. Incorporated that after some debugging.

--- R/mgcv.R
+++ R/mgcv.R
@@ -3,19 +3,27 @@
 ## Type 1 tensor product methods start here (i.e. Wood, 2006)
 #############################################################
 
-tensor.prod.model.matrix<-function(X)
+tensor.prod.model.matrix <- function(X) {
 # X is a list of model matrices, from which a tensor product model matrix is to be produced.
 # e.g. ith row is basically X[[1]][i,]%x%X[[2]][i,]%x%X[[3]][i,], but this routine works 
-# column-wise, for efficiency
-{ m<-length(X)
-  X1<-X[[m]]
-  n<-nrow(X1)
-  if (m>1) for (i in (m-1):1)
-  { X0<-X1;X1<-matrix(0,n,0)
-    for (j in 1:ncol(X[[i]]))
-    X1<-cbind(X1,X[[i]][,j]*X0)
-  }
-  X1
+# column-wise, for efficiency, and does work in compiled code.
+  m <- length(X)              ## number to row tensor product
+  d <- unlist(lapply(X,ncol)) ## dimensions of each X
+  n <- nrow(X[[1]])           ## columns in each X
+  X <- as.numeric(unlist(X))  ## append X[[i]]s columnwise
+  T <- numeric(n*prod(d))     ## storage for result
+  .C("mgcv_tensor_mm",
+     as.double(X),
+     as.double(T),
+     as.integer(d),
+     as.integer(m),
+     as.integer(n),
+     package="ma") ## produce product
+  ## Give T attributes of matrix. Note that initializing T as a matrix 
+  ## requires more time than forming the row tensor product itself (R 3.0.1)
+  attr(T,"dim") <- c(n,prod(d)) 
+  class(T) <- "matrix"
+  T
 } ## end tensor.prod.model.matrix
 
 uniquecombs<-function(x) {
---
+++ src/mgcv.h
@@ -0,0 +1,4 @@
+/* main method routines */
+#include <Rinternals.h>
+#include <Rconfig.h>
+void mgcv_tensor_mm(double *X,double *T,int *d,int *m,int *n);
